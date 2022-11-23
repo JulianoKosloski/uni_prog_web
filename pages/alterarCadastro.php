6817964a6280ce02bd1318d08a39aa863755b156
JulianoKosloski <juliano.koslos@gmail.com>
2022-11-23 13:22:19 -0300

    main nav include

--- pages/alterarCadastro.php
+++ pages/alterarCadastro.php
@@ -12,18 +12,10 @@
 </head> 
 <body>
     <div class="wrapper">
-        <nav class="main-nav" id="barraNav">
-            <ul class="logo">
-                <li><a href="home.php"><img id="logo-box" src="../images/favicon.ico"></a></li>
-                <li id="logo-title">Empresta já!</li>
-            </ul>
-            <ul class="nav-items">
-                <li><a href="home.php">Home</a></li>
-                <li><a href="alterarCadastro.php">Alterar cadastro</a></li>
-                <li><a href="listarEmprestimos.php">Meus empréstimos</a></li>
-                <li><a href="../login.php">Login</a></li>
-            </ul>
-        </nav>
+        
+        <?php 
+            include 'nav.php';
+        ?>
         
         <div class="box-container" id="formulario">
 
